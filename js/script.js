12d053790242a27c14a99f54014b1197e2635387
Amit Singh <65224005+babuaan@users.noreply.github.com>
2021-03-21 21:07:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+ $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        auto:true,
+        autoWidth:true,
+        loop:true,
+        
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
